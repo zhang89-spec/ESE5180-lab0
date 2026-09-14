a485fd08e6103dc0e472c14f5388a6424f6fbbc5
zhang89-spec <zhang89@seas.upenn.edu>
2026-09-14 18:01:16 -0400

    Add led5180 alias for LED2

--- Blinky/src/main.c
+++ Blinky/src/main.c
@@ -12,7 +12,7 @@
 #define SLEEP_TIME_MS   2000
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define LED0_NODE DT_ALIAS(led5180)
 
 /*
  * A build error on this line means your board is unsupported.
